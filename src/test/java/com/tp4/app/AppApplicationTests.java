f26c9b4526a3923594d583625d560de537de90d3
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2024-02-23 10:50:06 +0100

    Update AppApplicationTests.java

--- src/test/java/com/tp4/app/AppApplicationTests.java
+++ src/test/java/com/tp4/app/AppApplicationTests.java
@@ -1,13 +1,32 @@
 package com.tp4.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class AppApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppApplicationIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() {
+        // Ensure that the application context loads successfully
+    }
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
 }
